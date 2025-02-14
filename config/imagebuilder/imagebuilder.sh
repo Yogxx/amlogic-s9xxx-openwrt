6b8c973c3a0c5634f2a94fbe1eb14df72d2c8c88
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-02-14 18:24:30 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -196,13 +196,13 @@ rebuild_firmware() {
         kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant \
         libc php8 php8-cgi php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring \
         zoneinfo-core zoneinfo-asia nano htop unzip wget wget-ssl libmbedtls tar bash luci-theme-material \
-        netdata httping coreutils-timeout kmod-usb-net-rndis iperf3 \
+        netdata httping coreutils coreutils-timeout coreutils-nohup kmod-usb-net-rndis iperf3 \
         usbutils kmod-usb-net-cdc-ether kmod-usb-net-asix-ax88179 kmod-usb-net-asix-ax88179 usb-modeswitch \
         kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-qmi-wwan uqmi luci-proto-qmi \
         kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan qmi-utils \
         kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim umbim \
         libmbim libqmi modemmanager modemmanager-rpcd luci-proto-modemmanager \
-        kmod-tun kmod-nft-tproxy \
+        kmod-tun kmod-nft-tproxy vnstat2 vnstati2 luci-app-vnstat2 \
         perl perl-cgi perlbase-xsloader perl-http-date perlbase-utf8 openssh-sftp-server \
         \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
